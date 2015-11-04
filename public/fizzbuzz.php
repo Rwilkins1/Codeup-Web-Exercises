9fc350d00f4b97ade8d261747587da2d6cc86daf
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-04 15:39:48 -0600

    Refactored the fizzbuzz.php file for the for loop exercise and added the additional features from the exercise.

--- public/fizzbuzz.php
+++ public/fizzbuzz.php
@@ -1,8 +1,43 @@
 <?php
-$number = 1;
+$increment = 1;
+$start = 1;
+$end = 100;
 
-while ($number <= 100) {
-	if ($number % 15 == 0) {
+do {
+	fwrite(STDOUT, "Enter a Starting Number: ");
+	$start = trim(fgets(STDIN));
+	if (empty($start)) {
+		$start = 1;
+	} else if (!is_numeric($start)) {
+		echo "Please enter a valid number\n";
+	}
+} while (!is_numeric($start));
+
+do {
+	fwrite(STDOUT, "Enter an Ending Number: ");
+	$end = trim(fgets(STDIN));
+	if (empty($end)) {
+		$end = 100;
+	} else if(!is_numeric($end)) {
+		echo "Please enter a valid number\n";
+	}
+} while (!is_numeric($end));
+
+do {
+	fwrite(STDOUT, "Enter an Increment: ");
+	$increment = trim(fgets(STDIN));
+	if (empty($increment)) {
+		$increment = 1;
+	} else if (!is_numeric($increment)) {
+		echo "Please enter a valid number\n";
+	}
+} while (!is_numeric($increment));
+
+
+for ($number = $start; $number <= $end; $number += $increment) {
+	if ($number == 0) {
+		echo $number . "\n";
+	} else if ($number % 15 == 0) {
 		echo "Fizzbuzz\n";
 	} else if ($number % 3 == 0) {
 		echo "Fizz\n";
@@ -11,6 +46,5 @@ while ($number <= 100) {
 	} else {
 		echo $number . "\n";
 	}
-	$number++;
 }
 ?>
