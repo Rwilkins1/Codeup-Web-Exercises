3e2d5513481b51b705f5f73c3627ed160f3bca55
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-05 09:55:26 -0600

    completed the third step of the foreach exercise

--- public/foreach.php
+++ public/foreach.php
@@ -2,8 +2,14 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $misc) {
-	if(is_scalar($misc)) {
-		var_dump($misc);
+	if(is_array($misc)) {
+		echo "Array (";
+		foreach ($misc as $item) {
+			echo " $item ";
+		}
+		echo ")\n";
+	} else {
+	echo "$misc\n";
 	}
 }
 ?>
