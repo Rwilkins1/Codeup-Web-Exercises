004f1d9dd77ba923e8a993b578c8fea3d4a4f4ab
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-06 10:13:09 -0600

    completed the divide by zero portion of the exercise

--- public/arithmetic.php
+++ public/arithmetic.php
@@ -8,7 +8,7 @@ function add($a, $b)
 	if (is_numeric($a) && is_numeric($b)) {
     	return $a + $b;
     } else {
-    	return "Warning: Invalid Characters\n";
+    	return "Warning: Invalid Characters";
     }
 }
 
@@ -17,7 +17,7 @@ function subtract($a, $b)
 	if (is_numeric($a) && is_numeric($b)) {
     	return $a - $b;
 	} else {
-		return "Warning: Invalid Characters\n";
+		return "Warning: Invalid Characters";
 	}
 }
 
@@ -30,16 +30,20 @@ function multiply($a, $b)
 	    }
 	    return $i;
 	} else {
-		return "Warning: Invalid Characters\n";
+		return "Warning: Invalid Characters";
 	}
 }
 
 function divide($a, $b)
 {
 	if (is_numeric($a) && is_numeric($b)) {
-    	return $a / $b;
+		if($a == 0 || $b == 0) {
+			return "WHAT ARE YOU TRYING TO DO?!";
+		} else {
+    		return $a / $b;
+    	}
     } else {
-    	return "Warning: Invalid Characters\n";
+    	return "Warning: Invalid Characters";
     }
 }
 
@@ -48,13 +52,13 @@ function modulus($a, $b)
 	if (is_numeric($a) && is_numeric($b)) {
 		return $a % $b;
 	} else {
-		return "Warning: Invalid Characters\n";
+		return "Warning: Invalid Characters";
 	}
 }
 
 echo add($a, $b) . PHP_EOL;
 echo subtract($a, $b) . PHP_EOL;
-echo multiply(3, 4) . PHP_EOL;
-echo divide($a, $b) . PHP_EOL;
-echo modulus("Hello!", 2) . PHP_EOL;
+echo multiply("Hello!", 4) . PHP_EOL;
+echo divide($a, 0) . PHP_EOL;
+echo modulus(2, 2) . PHP_EOL;
 ?>
