22724fe2ca2c2ca8a43a3c89acb96815a9b8aa24
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-06 10:09:13 -0600

    completed the first step of the argument exercise

--- public/arithmetic.php
+++ public/arithmetic.php
@@ -5,36 +5,56 @@ $b = 10;
 
 function add($a, $b)
 {
-    return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a + $b;
+    } else {
+    	return "Warning: Invalid Characters\n";
+    }
 }
 
 function subtract($a, $b)
 {
-    return $a - $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a - $b;
+	} else {
+		return "Warning: Invalid Characters\n";
+	}
 }
 
 function multiply($a, $b)
 {
-	$i = 0;
-    for ($c = 0; $c < $b; $c++) {
-    	$i = $i + add($a, $a);
-    }
-    return $i;
+	if (is_numeric($a) && is_numeric($b)) {
+		$i = 0;
+	    for ($c = 0; $c < $b; $c++) {
+	    	$i = $i + add($a, $a);
+	    }
+	    return $i;
+	} else {
+		return "Warning: Invalid Characters\n";
+	}
 }
 
 function divide($a, $b)
 {
-    return $a / $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a / $b;
+    } else {
+    	return "Warning: Invalid Characters\n";
+    }
 }
 
 function modulus($a, $b)
 {
-	return $a % $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b;
+	} else {
+		return "Warning: Invalid Characters\n";
+	}
 }
 
 echo add($a, $b) . PHP_EOL;
 echo subtract($a, $b) . PHP_EOL;
 echo multiply(3, 4) . PHP_EOL;
 echo divide($a, $b) . PHP_EOL;
-echo modulus(6, 2) . PHP_EOL;
+echo modulus("Hello!", 2) . PHP_EOL;
 ?>
