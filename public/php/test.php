98dadd9cd7aa4e96876b39dfd22800af19d20f6d
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-06 10:35:38 -0600

    same as before

---
+++ public/php/test.php
@@ -0,0 +1,24 @@
+<?php
+$value = null;
+
+switch (gettype($value)) {
+    case 'integer':
+        echo '$value is an integer';
+        break;
+    case 'float':
+        echo '$value is a float';
+        break;
+    case 'boolean':
+        echo '$value is a boolean';
+        break;
+    case 'array':
+        echo '$value is an array';
+        break;
+    case 'null':
+        echo '$value is null';
+        break;
+    case 'string':
+        echo '$value is a string';
+        break;
+}
+?>
