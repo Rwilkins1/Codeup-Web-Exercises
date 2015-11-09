3d9cd407def354ee19ecaf9230bc6ef21402c32d
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-09 15:17:01 -0600

    placed my solution in a function and allowed users to alphabetize by way of argument

--- public/php/list_with_commas.php
+++ public/php/list_with_commas.php
@@ -1,16 +1,36 @@
 <?php
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 $physicistsarray = explode(', ', $physicistsString);
-$famousfakephysicists = [];
 
 
-for($i = 0; $i < count($physicistsarray); $i++) {
-	if($i == (count($physicistsarray)-1)) {
-		array_push($famousfakephysicists, "and " . $physicistsarray[$i]);
-	} else {
-		array_push($famousfakephysicists, $physicistsarray[$i]);
+function humanizedList($physicistsarray, $argc, $argv) {
+	$famousfakephysicists = [];
+	foreach ($physicistsarray as $index => $name) {
+		$physicistsarray[$index] = $name;
+		if($index == count($physicistsarray)-1) {
+			array_push($famousfakephysicists, "and " . $name);
+		} else {
+			array_push($famousfakephysicists, $name);
+		}
+		
 	}
+	if($argc == 2 && $argv[1] == "alphabetical") {
+		asort($famousfakephysicists);
+	}
+	
+	// for($i = 0; $i < count($physicistsarray); $i++) {
+	// 	asort($physicistsarray);
+	// 	if($i == (count($physicistsarray)-1)) {
+	// 		array_push($famousfakephysicists, "and " . $physicistsarray[$i]);
+	// 	} else {
+	// 		array_push($famousfakephysicists, $physicistsarray[$i]);
+	// 	}
+	// 	print_r($famousfakephysicists);
+	// }
+	$famousfakephysicists1 = implode(', ', $famousfakephysicists);
+	echo "Some of the most famous fictional theoretical physicists are $famousfakephysicists1.\n";
 }
-$famousfakephysicists1 = implode(', ', $famousfakephysicists);
-echo "Some of the most famous fictional theoretical physicists are $famousfakephysicists1.\n";
+
+humanizedlist($physicistsarray, $argc, $argv);
+
 ?>
