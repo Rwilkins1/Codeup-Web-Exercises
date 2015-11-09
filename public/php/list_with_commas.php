77efd21c1dd755c966b820e5104eac387ed3f375
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-09 16:01:48 -0600

    cleaned up the code

--- public/php/list_with_commas.php
+++ public/php/list_with_commas.php
@@ -1,26 +1,19 @@
 <?php
-$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark, Abraham Lincoln';
 $physicistsarray = explode(', ', $physicistsString);
 
-
 function humanizedList($physicistsarray, $argc, $argv) {
-	$famousfakephysicists = [];
-	foreach ($physicistsarray as $index => $name) {
-		$physicistsarray[$index] = $name;
-		if($index == count($physicistsarray)-1) {
-			array_push($famousfakephysicists, "and " . $name);
-		} else {
-			array_push($famousfakephysicists, $name);
-		}
-		
-	}
 	if($argc == 2 && $argv[1] == "alphabetical") {
-		asort($famousfakephysicists);
+		asort($physicistsarray);
 	}
-	
-	$famousfakephysicists1 = implode(', ', $famousfakephysicists);
-	echo "Some of the most famous fictional theoretical physicists are $famousfakephysicists1.\n";
+		$lastentry = 'and ' . array_pop($physicistsarray);
+		array_push($physicistsarray, $lastentry);
+		print_r($physicistsarray);
+		
+	$famousfakephysicists = implode(', ', $physicistsarray);
+	echo "Some of the most famous fictional theoretical physicists are $famousfakephysicists.\n";
 }
+
 
 humanizedlist($physicistsarray, $argc, $argv);
 
