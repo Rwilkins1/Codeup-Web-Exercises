7778d139bb43a204f9767eab01f155a365a58e45
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-10 15:06:03 -0600

    Cleaned up the code and generally bettered it.

--- public/php/contact-parser.php
+++ public/php/contact-parser.php
@@ -6,19 +6,33 @@ function parseContacts($filename)
 	$content = trim(fread($handle, filesize($filename)));
     $contactarray = explode("\n", $content);
     $contacts = array();
-    for ($i = 0; $i < count($contactarray); $i++) {
-    	$innercontacts = explode("|", $contactarray[$i]);
-    	$number1 = array_pop($innercontacts);
-    	$number2 = str_split($number1);
-    	array_splice($number2, 3, 0, '-');
-    	array_splice($number2, 7, 0, '-');
-    	$number3 = trim(implode("", $number2));
-    	array_push($innercontacts, $number3);
-    	array_push($contacts, $innercontacts);
+
+    foreach ($contactarray as $contactinfo) {
+        // Splits everything up
+    	$innercontacts = explode("|", $contactinfo);
+
+        // Dashes the phone numbers
+        $number = array_pop($innercontacts);
+    	$number1 = substr($number, 0, 3);
+        $number2 = substr($number, 3, 3);
+        $number3 = substr($number, 6, 4);
+        $phonenumber = "$number1-$number2-$number3";
+
+        // Puts the phone number back in
+        array_push($innercontacts, $phonenumber);
+
+        // Replaces the keys
+        $name = $innercontacts[0];
+        $phone = $innercontacts[1];
+        $keys = [
+            'Name' => $name, 
+            'Number' => $phone
+        ];
+        $contactstuff[] = $keys;
     	
     }
     fclose($handle);
-    return $contacts;
+    return $contactstuff;
 }
 
 var_dump(parseContacts('contacts.txt'));
