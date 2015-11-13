c84941dae2bf3f631c9303442d3eee7efadd1ce3
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-13 11:12:34 -0600

    started the first of the get request exercises

---
+++ public/php/counter.php
@@ -0,0 +1,13 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Counter</title>
+	<link rel="stylesheet" type="text/css" href="counter.css">
+</head>
+<body>
+	<a>Up</a>
+	<a>Down</a>
+</body>
+</html>
