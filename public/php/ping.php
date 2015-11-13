5538b85c082d17b83737fb3f3231c0bbca40fe0b
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-13 14:39:32 -0600

    added some additional styling

--- public/php/ping.php
+++ public/php/ping.php
@@ -11,10 +11,17 @@ extract(pagecontroller());
 <html>
 <head>
 	<title>Ping</title>
+	<link rel="stylesheet" type="text/css" href="pingpong.css">
 </head>
 <body>
-	<h2><div id = "score"><?=$count?></div></h2>
-	<a href="pong.php?val=<?=$count+1?>">Hit</a>
-	<a href="ping.php?val=<?=0?>">Miss</a>
+	<h2>Total Score<div id = "score"><?=$count?></div></h2>
+	<a id = "hit" href="pong.php?val=<?=$count+1?>">Hit</a>
+	<a id = "miss" href="ping.php?val=<?=0?>">Miss</a>
+	<br><br><br><br><br>
+	<img src="ping.png">
+<script type="text/javascript">
+	var audio1 = new Audio('grunt.wav');
+	audio1.play();
+</script>
 </body>
 </html>
