45646c65b54f43b7f1148244902e49e1bac47d26
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-13 13:27:16 -0600

    completed the counter exercise

--- public/php/counter.php
+++ public/php/counter.php
@@ -1,4 +1,11 @@
-<?php 
+<?php
+function pagecontroller() {
+	$count = isset($_GET['val']) ? $_GET['val'] : 0;
+	return array(
+		'count' => $count,
+	);
+}
+extract(pagecontroller());
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,7 +14,9 @@
 	<link rel="stylesheet" type="text/css" href="counter.css">
 </head>
 <body>
-	<a>Up</a>
-	<a>Down</a>
+	<h2>Counter:<div id = "counter"><?= $count ?></div></h2>
+	<a id = "up" href = "counter.php?val=<?=($count+1)?>">Up</a>
+	<a id = "down" href = "counter.php?val=<?=($count-1)?>">Down</a>
+	<a id = "home" href="counter.php">Home</a>
 </body>
 </html>
