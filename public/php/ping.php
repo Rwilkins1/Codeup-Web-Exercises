deaa2ab7e766475c29412fe8939a80aa8e1e0947
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-18 10:57:01 -0600

    edited the ping and pong to use the Input class

--- public/php/ping.php
+++ public/php/ping.php
@@ -1,6 +1,6 @@
 <?php
 	$count = isset($_GET['val']) ? $_GET['val'] : 0;
-	require 'functions.php';
+	require_once '../../Input.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,8 +9,8 @@
 	<link rel="stylesheet" type="text/css" href="pingpong.css">
 </head>
 <body>
-	<h2>Key Available: <?=inputhas('val')?></h2>
-	<h2>Total Score<div id = "score"><?=inputget('val')?></div></h2>
+	<h2>Key Available: <?=Input::has('val')?></h2>
+	<h2>Total Score<div id = "score"><?=Input::get('val')?></div></h2>
 	<a id = "hit" href="pong.php?val=<?=$count+1?>">Hit</a>
 	<a id = "miss" href="ping.php?val=<?=0?>">Miss</a>
 	<br><br><br><br><br>
