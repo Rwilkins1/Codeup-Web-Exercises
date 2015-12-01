5ba218c9c921e10c278fcd6a1035ab8ee76253a2
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-12-01 11:35:33 -0600

    added the pdo test file without my real password

---
+++ pdo_test.php
@@ -0,0 +1,8 @@
+<?php
+	define('DB_HOST', '127.0.0.1');
+	define('DB_NAME', 'employees');
+	define('DB_USER', 'codeup');
+	define('DB_PASS', '[my password]');
+	require 'db_connect.php';
+	echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . PHP_EOL;
+?>
