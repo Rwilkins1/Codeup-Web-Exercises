0c7d37e0431b1f90a7405685dd555f1dcf8a4587
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-12-02 10:11:31 -0600

    did the first step better

--- public/php/national_parks.php
+++ public/php/national_parks.php
@@ -2,6 +2,34 @@
 	require '../../parks_login.php';
 	require '../../db_connect.php';
 
-	$stmt = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC);
-	print_r($stmt);
+	$stmt = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC); 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>National Parks</title>
+</head>
+<body>
+	<h2>National Parks</h2>
+
+	<table>
+			<tr>	
+				<th>Park Name:</th>
+				<th>Location:</th>
+				<th>Date Established:</th>
+				<th>Area in Acres:</th>
+				
+			</tr>
+		<?php
+			foreach($stmt as $parkarray) { ?>
+			<tr>
+				<td><?=$parkarray['name']?></td>
+				<td><?=$parkarray['location']?></td>
+				<td><?=$parkarray['date_established']?></td>
+				<td><?=$parkarray['area_in_acres']?></td>
+			</tr>
+			<?php } ?>
+	</table>
+
+</body>
+</html>
