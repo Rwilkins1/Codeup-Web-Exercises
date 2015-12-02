842e61a74915f79edd5138ee025c62070a5132a7
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-12-02 10:53:09 -0600

    completed the pagination in its entirety.

--- public/php/national_parks.php
+++ public/php/national_parks.php
@@ -1,8 +1,19 @@
 <?php
 	require '../../parks_login.php';
 	require '../../db_connect.php';
-
-	$stmt = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC); 
+	$query1 = 'SELECT * FROM national_parks LIMIT 4';
+	$query2 = 'SELECT * FROM national_parks LIMIT 4 OFFSET 4';
+	$query3 = 'SELECT * FROM national_parks LIMIT 4 OFFSET 8';
+	$page = isset($_GET['page']) ? $_GET['page'] : 1; 
+	if(!isset($_GET['page'])) {
+		$stmt = $dbc->query($query1)->fetchAll(PDO::FETCH_ASSOC);
+	} else if ($_GET['page'] == 1) {
+		$stmt = $dbc->query($query1)->fetchAll(PDO::FETCH_ASSOC);
+	} else if($_GET['page'] == 2) {
+		$stmt = $dbc->query($query2)->fetchAll(PDO::FETCH_ASSOC);
+	} else if ($_GET['page'] == 3) {
+		$stmt = $dbc->query($query3)->fetchAll(PDO::FETCH_ASSOC);
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,6 +41,13 @@
 			</tr>
 			<?php } ?>
 	</table>
-
+	<?php
+		if(isset($_GET['page']) && $_GET['page'] != 1) { ?>
+			<a id = "previous" href="national_parks.php?page=<?=$page-1?>"><?="Previous Page"?></a>
+		<?php } ?>
+	<?php
+		if(!isset($_GET['page']) || $_GET['page'] < 3) { ?>
+			<a id = "next" href="national_parks.php?page=<?=$page+1?>"><?="Next Page"?></a>
+		<?php } ?>
 </body>
 </html>
