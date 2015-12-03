bf50e4a15ae2faf1ae5b6dacfb696ec1ded33472
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-12-03 14:27:38 -0600

    got the user input working

--- public/php/national_parks.php
+++ public/php/national_parks.php
@@ -1,7 +1,10 @@
 <?php
-	// require '../../parks_login.php';
-	// require '../../db_connect.php';
-	require '../../park_seeder.php';
+	require '../../parks_login.php';
+	require '../../db_connect.php';
+	// require '../../park_seeder.php';
+	require '../../input.php';
+
+
 	$query = $dbc->prepare('SELECT * FROM national_parks LIMIT :lim OFFSET :off');
 	$page = isset($_GET['page']) ? round($_GET['page']) : 1; 
 	// $maxpage = ($dbc->rowCount())/4;
@@ -15,6 +18,41 @@
 	$query->bindValue(':off', $offset, PDO::PARAM_INT);
 	$query->fetchAll(PDO::FETCH_ASSOC);
 	$query->execute();
+
+	// $name = isset($_POST['parkname']) ? $_POST['parkname'] : '';
+	// $location = isset($_POST['parklocation']) ? $_POST['parklocation'] : '';
+	// $date = isset($_POST['date']) ? $_POST['date'] : '';
+	// $area = isset($_POST['area']) ? $_POST['area'] : '';
+	// $description = isset($_POST['parkdescription']) ? $_POST['parkdescription'] : '';
+
+	function insertpark($dbc)
+	{
+		$name = Input::get('parkname');
+		$location = Input::get('parklocation');
+		$date = Input::get('date');
+		$area = Input::get('area');
+		$description = Input::get('parkdescription');
+
+		$inner = 'INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)';
+		$query = $dbc->prepare($inner);
+		$query->bindValue(':name', $name, PDO::PARAM_STR);
+		$query->bindValue(':location', $location, PDO::PARAM_STR);
+		$query->bindValue(':date_established', $date, PDO::PARAM_STR);
+		$query->bindValue(':area_in_acres', $area, PDO::PARAM_STR);
+		$query->bindValue(':description', $description, PDO::PARAM_STR);
+		$query->fetchAll(PDO::FETCH_ASSOC);
+		$query->execute();
+		var_dump($_POST);
+	}
+		// $stmt = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)');
+		// $stmt->fetchAll(PDO::FETCH_ASSOC);
+		// $stmt->execute(array(':name' => $name, ':location' => $location, ':date_established' => $date, ':area_in_acres' => $area, ':description' => $description));
+		// header('location: national_parks.php');
+
+	if(Input::notempty('parkname') && Input::notempty('parklocation') && Input::notempty('date') && Input::notempty('area') && Input::notempty('parkdescription')) {
+		insertpark($dbc);
+	}
+
 	// $stmt = $dbc->query($query . $offset)->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
@@ -102,5 +140,31 @@
 		if(isset($_GET['page']) && $_GET['page'] == 3) { ?>
 			<a id = "home" href = "national_parks.php">Home</a>
 		<?php } ?>
+
+		<h2>Didn't find the Park you were looking for?</h2>
+		<h3>Fill in the information below</h3>
+		<form method = "POST">
+			<p>
+				<label for "parkname">Park Name</label>
+				<input type = "text" id = "parkname" name = "parkname">
+			</p>
+			<p>
+				<label for "parklocation">Location</label>
+				<input type = "text" id = "parklocation" name = "parklocation">
+			</p>
+			<p>
+				<label for "date">Date Established</label>
+				<input type = "text" id = "date" name = "date">
+			</p>
+			<p>
+				<label for "area">Area (in Acres)</label>
+				<input type = "text" id = "area" name = "area">
+			</p>
+			<p>
+				<label for "parkdescription">Description</label>
+				<textarea id = "parkdescription" name = "parkdescription">Enter a short description of the park</textarea>
+			</p>
+			<button type = "submit" value = "submit">Submit</button>
+		</form>
 </body>
 </html>
