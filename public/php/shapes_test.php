2a5af991a808f6fdcad3460beedad7c4e7cc80de
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-12-04 11:29:07 -0600

    tested the area and perimeter methods

--- public/php/shapes_test.php
+++ public/php/shapes_test.php
@@ -3,14 +3,16 @@
 	require_once 'square.php';
 	$rectangle1 = new Rectangle(3, 5);
 	echo $rectangle1->area() . PHP_EOL;
+	echo $rectangle1->perimeter() . PHP_EOL;
 	$rectangle2 = new Rectangle(10,150);
 	echo $rectangle2->area() . PHP_EOL;
+	echo $rectangle2->perimeter() . PHP_EOL;
 
-	$square1 = new Square(5, 5);
+	$square1 = new Square(5);
 	echo $square1->area() . PHP_EOL;
 	echo $square1->perimeter() . PHP_EOL;
 
-	$square2 = new Square(10, 10);
+	$square2 = new Square(10);
 	echo $square2->area() . PHP_EOL;
 	echo $square2->perimeter() . PHP_EOL;
 ?>
