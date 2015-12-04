bf1a9a72a62d700f848ab42edd29a3411707afdf
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-12-04 11:18:07 -0600

    overwrote the parent constructor

--- public/php/square.php
+++ public/php/square.php
@@ -1,6 +1,16 @@
 <?php
+
+require_once 'rectangle.php';
+
 class Square extends Rectangle  
 {
+	public function __construct($height)
+	{
+		$this->height = $height;
+		$this->width = $height;
+	}
+
+	
 	public function perimeter()
 	{
 		return 4 * $this->height;
