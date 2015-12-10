bf814ca81193494c1d5deb85374126b55a3268ee
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-12-10 09:27:42 -0600

    added the getString($key) method

---
+++ Input.php
@@ -0,0 +1,62 @@
+<?php
+
+class Input
+{
+    /**
+     * Check if a given value was passed in the request
+     *
+     * @param string $key index to look for in request
+     * @return boolean whether value exists in $_POST or $_GET
+     */
+
+    public static function notempty($key)
+    {
+        if(isset($_REQUEST[$key]) && $_REQUEST[$key] != '') {
+            return true;
+        }
+    }
+
+
+    public static function has($key)
+    {
+        if(isset($_REQUEST[$key])) {
+            return "true" . PHP_EOL;
+        } else {
+            return "false" . PHP_EOL;
+        }
+    }
+
+    public static function getString($key)
+    {
+        $input = self::get($key);
+        if (!is_string($input)) {
+            throw new Exception('Your input must be a string!');
+        }
+    }
+
+    /**
+     * Get a requested value from either $_POST or $_GET
+     *
+     * @param string $key index to look for in index
+     * @param mixed $default default value to return if key not found
+     * @return mixed value passed in request
+     */
+    public static function get($key, $default = null)
+    {
+        if(isset($_REQUEST[$key])) {
+            return $_REQUEST[$key];
+        } else if (isset($_SESSION[$key])){
+            return $_SESSION[$key];
+        } else {
+            return 0;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //                      DO NOT EDIT ANYTHING BELOW!!                     //
+    // The Input class should not ever be instantiated, so we prevent the    //
+    // constructor method from being called. We will be covering private     //
+    // later in the curriculum.                                              //
+    ///////////////////////////////////////////////////////////////////////////
+    private function __construct() {}
+}
