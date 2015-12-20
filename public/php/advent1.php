c9d2af794680475fc1f6ff9ee31a7f13f9d29e69
Reagan Wilkins <reagan.wilkins@gmail.com>
2015-12-20 14:17:47 -0600

    completed day 1 of advent of code

--- public/php/advent1.php
+++ public/php/advent1.php
@@ -5,12 +5,20 @@ function parser($filename)
 	$content = trim(fread($handle, filesize($filename)));
 	$parentharray = str_split($content);
 	$floor = 0;
+	// $position = 0;
 
-	foreach ($parentharray as $parenthesis) {
-		if ($parenthesis == "(") {
-			$floor += 1;
-		} else if ($parenthesis == ")") {
-			$floor -= 1;
+	foreach ($parentharray as $position => $parenthesis) {
+		// var_dump ($parentharray);
+		if ($floor == -1) {
+			echo $floor . PHP_EOL;
+			echo $position . PHP_EOL;
+			return $position;
+		} else {
+			if ($parenthesis == "(") {
+				$floor += 1;
+			} else if ($parenthesis == ")") {
+				$floor -= 1;
+			}
 		}
 	}
 
