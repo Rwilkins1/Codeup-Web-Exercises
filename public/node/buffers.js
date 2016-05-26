d8f261168ceeadcdd036de4422a733509b0e1cdc
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-05-26 13:49:48 -0500

    started the streams exercise for node.js

--- public/node/buffers.js
+++ public/node/buffers.js
@@ -1,3 +1,2 @@
-var buffer1 = new Buffer("TutorialsPoint");
-var buffer2 = buffer1.slice(0,9);
-console.log("buffer2 content: " + buffer2.toString());
+var buffer = new Buffer('TutorialsPoint');
+console.log("buffer length: " + buffer.length);
