553d5e9432c37b29373f468b431067cefb9988c0
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-05-26 14:03:32 -0500

    actually decompressed the input.txt file

--- public/node/streams.js
+++ public/node/streams.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var zlib = require('zlib');
 
-fs.createReadStream('input.txt').pipe(zlib.createGzip()).pipe(fs.createWriteStream('input.txt.gz'));
+fs.createReadStream('input.txt.gz').pipe(zlib.createGunzip()).pipe(fs.createWriteStream('input.txt'));
 
-console.log("File Compressed");
+console.log("File Decompressed");
