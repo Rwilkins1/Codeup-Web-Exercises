80ed0caa169003f36a83cb068ef4e36fdfac36c3
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-05-31 10:41:07 -0500

    completed the "GET" request exercise

--- public/node/express.js
+++ public/node/express.js
@@ -3,8 +3,17 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-	res.send("Hello World!");
+app.get('/index.htm', function(req, res) {
+	res.sendFile(__dirname + "/" + "index.htm");
+});
+
+app.get('/process_get', function(req, res) {
+	response = {
+		first_name:req.query.first_name,
+		last_name:req.query.last_name
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
 });
 
 var server = app.listen(8081, function() {
