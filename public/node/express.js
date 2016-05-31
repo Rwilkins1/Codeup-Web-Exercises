2f19137143356f6ab11436b56b4ae884e5b0d8fe
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-05-31 11:05:34 -0500

    completed the "POST" requests exercise

--- public/node/express.js
+++ public/node/express.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
 
 app.use(express.static('public'));
 
@@ -7,10 +10,10 @@ app.get('/index.htm', function(req, res) {
 	res.sendFile(__dirname + "/" + "index.htm");
 });
 
-app.get('/process_get', function(req, res) {
+app.post('/process_post', urlencodedParser, function(req, res) {
 	response = {
-		first_name:req.query.first_name,
-		last_name:req.query.last_name
+		first_name:req.body.first_name,
+		last_name:req.body.last_name
 	};
 	console.log(response);
 	res.end(JSON.stringify(response));
@@ -19,6 +22,5 @@ app.get('/process_get', function(req, res) {
 var server = app.listen(8081, function() {
 	var host = server.address().address
 	var port = server.address().port
-
 	console.log("Example app listening at http://%s:%s", host, port)
 });
