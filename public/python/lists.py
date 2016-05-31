886258a1ccc66c229894a5a0d9e2347335df808f
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-05-31 14:45:16 -0500

    completed the "lists" exercise

---
+++ public/python/lists.py
@@ -0,0 +1,20 @@
+list = ['abcd', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+
+# Prints complete list
+print(list)
+
+# Prints first element of the list
+print(list[0])
+
+# Prints elemtents starting from 2nd till 3rd
+print(list[1:3])
+
+# Prints elements starting from the third
+print(list[2:])
+
+# Prints the list twice
+print(tinylist * 2)
+
+# Prints concatenated lists
+print(list + tinylist)
