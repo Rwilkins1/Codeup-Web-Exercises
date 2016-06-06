60b23c2344b3bcbc76226ec3aeceab2fc03df94d
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 14:28:30 -0500

    completed the default arguments exercise

--- public/python/functions.py
+++ public/python/functions.py
@@ -1,5 +1,7 @@
-def printinfo(name, age):
+def printinfo(name="Billybob", age="35"):
 	print name + " is " + age + " years old";
 	return;
 
 printinfo(age="50", name="Miki");
+printinfo(name="Miki");
+printinfo();
