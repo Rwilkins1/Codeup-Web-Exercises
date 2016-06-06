453a274de38e9de6e97cb64baf43b5d4858401de
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 15:27:55 -0500

    completed the write() exercise

--- public/python/files.py
+++ public/python/files.py
@@ -1,5 +1,4 @@
 fo = open("foo.txt", "wb")
-print "Name of the file: ", fo.name;
+fo.write("Python is a great language.\nYeah, it is really great!\n");
 
 fo.close();
-print "Is it closed?: ", fo.closed;
