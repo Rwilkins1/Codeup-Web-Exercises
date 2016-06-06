a73002e695cc252a74cad6210dde321979a905be
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 11:31:33 -0500

    completed the triple quotes exercise

--- public/python/strings.py
+++ public/python/strings.py
@@ -1,4 +1,8 @@
-name = "Reagan"
-weight = 180
+paragraph = """this is a long string that is made up of
+several lines and non-printable characters such as
+TAB ( \t ) and they will show up that way when displayed. 
+NEWLINEs within the string, whether explicitly given like 
+this within the brackets [ \n ], or just a NEWLINE within the variable assignment will also show up.
+"""
 
-print "My name is %s and my weight is %d lbs!" % (name, weight)
+print paragraph
