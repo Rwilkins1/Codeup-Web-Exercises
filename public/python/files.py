b089b95f7b970dc6ccf742c223c14d0410e978cb
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 15:23:04 -0500

    completed the close() exercise

--- public/python/files.py
+++ public/python/files.py
@@ -1,5 +1,5 @@
 fo = open("foo.txt", "wb")
 print "Name of the file: ", fo.name;
-print "Is it closed: ", fo.closed;
-print "Opening Mode: ", fo.mode;
-print "Softspace flag: ", fo.softspace;
+
+fo.close();
+print "Is it closed?: ", fo.closed;
