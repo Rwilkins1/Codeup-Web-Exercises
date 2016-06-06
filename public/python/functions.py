6e22c09dc2713ce812299cf07d65cd5dd9be54b2
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 14:45:47 -0500

    completed the scope of variables exercise

--- public/python/functions.py
+++ public/python/functions.py
@@ -1,7 +1,9 @@
+total = 0;
+
 def sum(arg1, arg2):
-	total = arg1 + arg2
-	print "Inside the function: ", total
+	total = arg1 + arg2;
+	print "Inside the function local total: ", total
 	return total;
 
-total = sum(10, 20);
-print "Outside the function: ", total
+sum(10, 20)
+print "Outside the function global total: ", total
