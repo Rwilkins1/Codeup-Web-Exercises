a9d91eb83eef38fce46e047958a9bef0ff5a75db
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 14:07:19 -0500

    completed another functions exercise

--- public/python/functions.py
+++ public/python/functions.py
@@ -1,8 +1,8 @@
 def changeme(list):
-	list.append([1,2,3,4]);
-	print "Values inside the function: ", list
-	return
+	list = [1,2,3,4];
+	print "Values inside the function: ", list;
+	return;
 
-list = [10,20,30];
+list = [10,20,30]
 changeme(list);
-print "Values outside the function: ", list
+print "Values outside the function: ", list;
