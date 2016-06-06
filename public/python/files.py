db463b1a0b3a29d64d39e0eaa3b3e4348749889e
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 15:33:22 -0500

    completed the file positions exercise

--- public/python/files.py
+++ public/python/files.py
@@ -1,6 +1,13 @@
 fo = open("foo.txt", "r+")
 str = fo.read(10);
-
 print "Read String is: ", str
+
+position = fo.tell();
+print "current file position: ", position
+
+position = fo.seek(0, 0);
+str = fo.read(10);
+
+print "Again read String is: ", str;
 
 fo.close();
