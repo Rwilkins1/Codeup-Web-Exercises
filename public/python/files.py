cc18e464bf25cf739ed3743936456ef5aa475b3b
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 15:21:37 -0500

    completed the file opening exercise

--- public/python/files.py
+++ public/python/files.py
@@ -1,2 +1,5 @@
-str = input("Enter your input: ");
-print "Received input is: ", str;
+fo = open("foo.txt", "wb")
+print "Name of the file: ", fo.name;
+print "Is it closed: ", fo.closed;
+print "Opening Mode: ", fo.mode;
+print "Softspace flag: ", fo.softspace;
