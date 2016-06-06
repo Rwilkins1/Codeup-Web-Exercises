fd55a207fcb6eaf7649470edd5d2a519ae6c5da1
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-06 13:51:12 -0500

    completed the first function exercise

---
+++ public/python/functions.py
@@ -0,0 +1,7 @@
+def printme(str):
+	"This prints a passed string into this function"
+	print str
+	return;
+
+printme("I'm the first call to the function!")
+printme("I'm the second call to the function!")
