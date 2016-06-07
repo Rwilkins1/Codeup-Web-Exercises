f892fa40e58ea400c2b828ce24a4278d772ff4c8
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-07 15:35:48 -0500

    completed the searching exercise

--- public/python/reg.py
+++ public/python/reg.py
@@ -1,12 +1,12 @@
 import re
 
-line = "Dogs are better than cats"
+line = "Dogs are better than cats";
 
-matchObj = re.match(r'(.*) are (.*?) .*', line, re.M|re.I)
+searchObj = re.search(r'(.*) are (.*?) .*', line, re.M|re.I)
 
-if matchObj:
-	print "matchObj.group(): ", matchObj.group()
-	print "matchObj.group(1): ", matchObj.group(1)
-	print "matchObj.group(2): ", matchObj.group(2)
+if searchObj:
+	print "searchObj.group(): ", searchObj.group();
+	print "searchObj.group(1): ", searchObj.group(1);
+	print "searchObj.group(2): ", searchObj.group(2);
 else:
-	print "No match!!"
+	print "Nothing found!"
