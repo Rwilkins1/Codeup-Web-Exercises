54d10bad3c13ebf15f1f84181768942357090771
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-07 13:26:08 -0500

    completed the second classes exercise

--- public/python/classes.py
+++ public/python/classes.py
@@ -9,3 +9,12 @@ emp1.displayEmployee()
 emp2.displayEmployee()
 
 print "Total Employee %d" % Employee.empCount
+emp1.age = 24
+
+print hasattr(emp1, 'age')
+print getattr(emp1, 'age')
+print setattr(emp1, 'age', 25)
+print hasattr(emp1, 'age')
+print getattr(emp1, 'age')
+print delattr(emp1, 'age')
+print hasattr(emp1, 'age')
