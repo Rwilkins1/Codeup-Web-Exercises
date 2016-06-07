f2c77bc56d86e1f420023f2fb67fc620ebfb4f8a
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-07 13:22:18 -0500

    completed the first class exercise

--- public/python/classes.py
+++ public/python/classes.py
@@ -1,5 +1,11 @@
-import Employee
+from Employee import * 
 
 emp1 = Employee("Zara", 2000)
 
 emp2 = Employee("Manni", 5000)
+
+emp1.displayEmployee()
+
+emp2.displayEmployee()
+
+print "Total Employee %d" % Employee.empCount
