c994f41e2519ccb565dbb6e8c95e149b0d5406b9
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-07 09:27:36 -0500

    completed the second half of the try-finally exercise

--- public/python/exceptions.py
+++ public/python/exceptions.py
@@ -1,5 +1,9 @@
 try:
-	fh = open("test.txt", "w")
-	fh.write("This is my test file for exception handling!")
-finally:
+	fh = open("testfile.txt", "w")
+	try:
+		fh.write("This is my test file for exception handling!!")
+	finally:
+		print "Going to close the file"
+		fh.close()
+except IOError:
 	print "Error: cannot find file or read data"
