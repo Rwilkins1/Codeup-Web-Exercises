27f0bda990efc0c16e80043bbf91a96ae8186be7
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-08 15:59:23 -0500

    completed the hash variables exercise

--- public/perl/var.pl
+++ public/perl/var.pl
@@ -1,14 +1,7 @@
-# Array Variables Exercise
-$var1 = 25;
-$var2 = 30;
-$var3 = 40;
-@ages = ($var1, $var2, $var3);
-@names = ("Paul", "George", "Ringo", "John");
+# Hash Variables Exercise
 
-# Use Escape characters to just print the word $ages, otherwise it will be interpreted
-print "\$ages[0] = $ages[0]\n";
-print "\$ages[1] = $ages[1]\n";
-print "\$ages[2] = $ages[2]\n";
-print "\$names[0] = $names[0]\n";
-print "\$names[1] = $names[1]\n";
-print "\$names[2] = $names[2]\n";
+%data = ('Best Beatle', 'John', 'Worst Beatle', 'Ringo', 'Meh Beatle', 'George');
+
+print "\$data{'Best Beatle'} = $data{'Best Beatle'}\n";
+print "\$data{'Meh Beatle'} = $data{'Meh Beatle'}\n";
+print "\$data{'Worst Beatle'} = $data{'Worst Beatle'}\n";
