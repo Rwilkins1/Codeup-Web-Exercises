7de4e8c000ba86d6ab8da43e9ce601111bf3843e
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-08 15:34:31 -0500

    completed the first datatypes exercise

---
+++ public/perl/datatypes.pl
@@ -0,0 +1,17 @@
+$str = "Welcome to \ntutorialspoint.com";
+print "$str\n";
+
+$str = 'Welcome to tutorialspoint.com';
+print "$str\n";
+
+$str = "\uwelcome to tutorialspoint.com";
+print "$str\n";
+
+$str = "\UWelcome to tutorialspoint.com";
+print "$str\n";
+
+$str = "Welcome to \Ututorialspoint.com";
+print "$str\n";
+
+$str = "\QWelcome to tutorialspoint.com's family";
+print "$str\n";
