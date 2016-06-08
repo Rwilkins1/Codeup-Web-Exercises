c1831d459443cd60369a7ba937065bdf49022e42
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-08 15:56:20 -0500

    completed the array variables exercise

--- public/perl/var.pl
+++ public/perl/var.pl
@@ -1,7 +1,14 @@
-$age = 25;
-$name = "John Paul";
-$salary = 1445.50;
+# Array Variables Exercise
+$var1 = 25;
+$var2 = 30;
+$var3 = 40;
+@ages = ($var1, $var2, $var3);
+@names = ("Paul", "George", "Ringo", "John");
 
-print "Age: $age\n";
-print "Name: $name\n";
-print "Salary: $salary\n";
+# Use Escape characters to just print the word $ages, otherwise it will be interpreted
+print "\$ages[0] = $ages[0]\n";
+print "\$ages[1] = $ages[1]\n";
+print "\$ages[2] = $ages[2]\n";
+print "\$names[0] = $names[0]\n";
+print "\$names[1] = $names[1]\n";
+print "\$names[2] = $names[2]\n";
