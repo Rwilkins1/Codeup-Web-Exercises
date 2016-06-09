475c6c047c3767e16c00adacec0e1e27705235da
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-09 09:58:30 -0500

    completed the special literals exercise

--- public/perl/scalar.pl
+++ public/perl/scalar.pl
@@ -1,11 +1,6 @@
-# V-strings exercise
+# Special literals exercise
 
-$smile = v9786;
-$foo = v102.111.111;
-$martin = v77.97.114.116.105.110;
-$reagan = v82.101.97.103.97.110;
+print "File name " . __FILE__ . "\n";
+print "Line number " . __LINE__ . "\n";
+print "Package " . __PACKAGE__ . "\n";
 
-print "Smile = $smile\n";
-print "Foo = $foo\n";
-print "Martin = $martin\n";
-print "Reagan = $reagan\n";
