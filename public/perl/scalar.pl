f94b6cd9ad5698aa01c7198978e8133f9f1e2561
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-09 09:42:11 -0500

    completed the v-string exercise, and did my name with v-strings in addition

--- public/perl/scalar.pl
+++ public/perl/scalar.pl
@@ -1,7 +1,11 @@
-# Multiline Strings exercise - "Here" document syntax
+# V-strings exercise
 
-print <<EOF;
-This is undoubtedly a
-multiple lined
-string
-EOF
+$smile = v9786;
+$foo = v102.111.111;
+$martin = v77.97.114.116.105.110;
+$reagan = v82.101.97.103.97.110;
+
+print "Smile = $smile\n";
+print "Foo = $foo\n";
+print "Martin = $martin\n";
+print "Reagan = $reagan\n";
