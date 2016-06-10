c2194e8a8fa0a1995757d7afe0efcd909d024a19
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-10 13:30:17 -0500

    completed the accessing hash values exercise

---
+++ public/perl/hashes.pl
@@ -0,0 +1,8 @@
+# Accessing Hash Elements
+%data = ("Best Beatle" => "John", "Meh Beatle" => "George", "Worst Beatle" => "Ringo");
+
+print "The best Beatle is $data{'Best Beatle'}\n";
+
+print "The meh Beatle is $data{'Meh Beatle'}\n";
+
+print "The worst Beatle is $data{'Worst Beatle'}\n";
