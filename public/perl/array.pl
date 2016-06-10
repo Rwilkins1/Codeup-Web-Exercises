12f15786a9f7afef953d9367abaac77c1b2c9836
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-10 11:24:04 -0500

    Completed the slicing exercise

--- public/perl/array.pl
+++ public/perl/array.pl
@@ -1,21 +1,6 @@
-# Adding and Removing elements
+# Array Slicing
 
-# Array creation
-@dogs = ("Black Labrador", "Border Collie", "Dachsund", "German Shepherd");
-print "1. \@dogs = @dogs\n";
+@days = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
+@weekend = @days[5, 6];
 
-# Add one element at the end of the array
-push(@dogs, "Chihuahua");
-print "2. \@dogs = @dogs\n";
-
-# Add one element to the beginning of the array
-unshift(@dogs, "Pit Bull");
-print "3. \@dogs = @dogs\n";
-
-# Remove one element from the last of the array
-pop(@dogs);
-print "4. \@dogs = @dogs\n";
-
-# Remove one element from the beginning of the array
-shift(@dogs);
-print "5. \@dogs = @dogs\n";
+print "@weekend\n";
