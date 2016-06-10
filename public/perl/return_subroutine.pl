d976e9373b1068d08aed480354c01c2c6dfbc4eb
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-10 15:26:37 -0500

    completed the returning values from subroutines exercise

---
+++ public/perl/return_subroutine.pl
@@ -0,0 +1,14 @@
+sub Average{
+	$n = scalar(@_);
+	$sum = 0;
+
+	foreach $item(@_) {
+		$sum += $item;
+	}
+	$average = $sum / $n;
+
+	return $average;
+}
+
+$num = Average(10, 20, 30, 40, 50, 60);
+print "Average for the given numbers: $num\n";
