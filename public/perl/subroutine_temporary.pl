c518ccbadfa40bb3742b1763e248773768b59c04
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-10 15:34:03 -0500

    completed the temporary variables exercise

---
+++ public/perl/subroutine_temporary.pl
@@ -0,0 +1,15 @@
+$string = "Hello, World!";
+
+sub PrintHello{
+	local $string;
+	$string = "Hello, Perl!";
+	PrintMe();
+	print "Inside the function PrintHello $string\n";
+}
+
+sub PrintMe{
+	print "Inside the function PrintMe $string\n";
+}
+
+PrintHello();
+print "Outside the function $string\n";
