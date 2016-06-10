6583ecdd28be721b63b6fbc7a381edf101154889
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-10 10:53:12 -0500

    completed one of the array exercises in perl, came up with JavaScript quesitons, and created a visual representation of a concept I was trying to explain.

--- public/perl/array.pl
+++ public/perl/array.pl
@@ -1,4 +1,4 @@
-# Array Creation Exercise
+# Accessing Array Elements
 @array = qw/
 Monday
 Tuesday
@@ -9,4 +9,9 @@ Saturday
 Sunday
 /;
 
-print @array;
+print "$array[0]\n";
+print "$array[1]\n";
+print "$array[2]\n";
+print "$array[6]\n";
+print "$array[-1]\n";
+print "$array[-7]\n";
