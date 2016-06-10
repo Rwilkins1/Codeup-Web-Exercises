195feee5cb2990dc78379773492c63b6381125ab
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-10 13:36:39 -0500

    completed the extracting keys and values exercise

--- public/perl/hashes.pl
+++ public/perl/hashes.pl
@@ -1,7 +1,11 @@
-# Extracting Slices
+# Extracting Keys and values
 
-%data = (-JohnPaul => 45, -Lisa => 30, -Kumar => 40);
+%data = ('JohnPaul' => 45, 'Lisa' => 30, 'Kumar' => 40);
 
-@array = @data{-JohnPaul, -Lisa};
+@names = keys %data;
 
-print "Array: @array\n";
+@ages = values %data;
+
+print "$names[0] is $ages[0] years old\n";
+print "$names[1] is $ages[1] years old\n";
+print "$names[2] is $ages[2] years old\n";
