3c7e86bec9aab2d94d9ed44ada38dc7930f9ec07
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-10 13:11:22 -0500

    completed the turning strings to arrays exercise

--- public/perl/array.pl
+++ public/perl/array.pl
@@ -1,6 +1,10 @@
-# Replacing Array Elements
-@nums = (1..20);
-print "Before - @nums\n";
+# Transform Strings to Arrays
 
-splice(@nums, 5, 5, 21..25);
-print "After - @nums\n";
+$string = "Rain-Drops-On-Roses-And-Whiskers-On-Kittens";
+$names = "Larry,David,Roger,Ken,Michael,Tom";
+
+@string = split('-', $string);
+@names = split(',', $names);
+
+print "$string[3]\n";
+print "$names[4]\n";
